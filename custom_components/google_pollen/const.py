b7586559af6101c875e4cad4525c4e1a36ac3366
Sven-Ove Bjerkan <github.com@sven-ove.no>
2025-03-11 21:05:40 +0100

    Update const.py

--- custom_components/google_pollen/const.py
+++ custom_components/google_pollen/const.py
@@ -4,3 +4,7 @@ DOMAIN = "google_pollen"
 DEFAULT_LANGUAGE = "en"
 
 BASE_URL = "https://pollen.googleapis.com/v1/forecast:lookup"
+
+PLANT_TYPES = ["BIRCH", "HAZEL", "ALDER", "MUGWORT", "ASH", "COTTONWOOD", "OAK", "PINE", "OLIVE", "GRAMINALES", "RAGWEED", "ELM", "MAPLE", "JUNIPER", "CYPRESS_PINE", "JAPANESE_CEDAR"]
+
+CONF_POLLEN = "conf_pollen"
