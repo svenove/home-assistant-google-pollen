e2d48647f57759877492282cfd49470a54d7d66a
Sven-Ove Bjerkan <github.com@sven-ove.no>
2025-03-11 23:26:39 +0100

    Update utils.py

--- custom_components/google_pollen/utils.py
+++ custom_components/google_pollen/utils.py
@@ -1,9 +1,12 @@
-import aiohttp
+"""Utility functions for Google Pollen integration."""
 import logging
+
+import aiohttp
 
 from .const import BASE_URL
 
 _LOGGER = logging.getLogger(__name__)
+
 
 async def fetch_pollen_data(api_key, latitude, longitude, language, days=1):
     """Fetch pollen data from the API."""
@@ -14,13 +17,12 @@ async def fetch_pollen_data(api_key, latitude, longitude, language, days=1):
             "location.longitude": longitude,
             "languageCode": language,
             "days": days,
-            "plantsDescription": "false"
+            "plantsDescription": "false",
         }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(BASE_URL, params=params) as response:
+        async with aiohttp.ClientSession() as session, session.get(BASE_URL, params=params) as response:
                 response.raise_for_status()
                 data = await response.json()
-        
+
         _LOGGER.debug("API-result: %s", data)
         return data
 
