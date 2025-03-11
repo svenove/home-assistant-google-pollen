670393568f3880b459e115a811b0ac87fb440ad5
Sven-Ove Bjerkan <github.com@sven-ove.no>
2025-03-11 17:57:47 +0100

    Create utils.py

---
+++ custom_components/google_pollen/utils.py
@@ -0,0 +1,33 @@
+import aiohttp
+import logging
+
+from .const import BASE_URL
+
+_LOGGER = logging.getLogger(__name__)
+
+async def fetch_pollen_data(api_key, latitude, longitude, language, days=1):
+    """Fetch pollen data from the API."""
+    try:
+        params = {
+            "key": api_key,
+            "location.latitude": latitude,
+            "location.longitude": longitude,
+            "languageCode": language,
+            "days": days,
+            "plantsDescription": "false"
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(BASE_URL, params=params) as response:
+                response.raise_for_status()
+                data = await response.json()
+        
+        _LOGGER.debug("API-result: %s", data)
+        return data
+
+    except aiohttp.ClientResponseError as error:
+        _LOGGER.error("Error fetching pollen data: %s", error)
+        raise
+
+    except Exception as error:
+        _LOGGER.error("Unexpected error: %s", error)
+        raise
